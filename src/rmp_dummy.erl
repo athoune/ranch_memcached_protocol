b06ae884f74c58a1c55a5f19e515f7441695fa1e
Mathieu Lecarme <mathieu@garambrogne.net>
2012-12-05 00:11:58 +0100

    More get.

--- src/rmp_dummy.erl
+++ src/rmp_dummy.erl
@@ -5,10 +5,12 @@
 -include("rmp_constants.hrl").
 
 handle(Conn, ?SET, #rmp_message{}=Message) ->
-    io:format("Handle ~p~n", [Message]),
+    io:format("Handle SET ~p~n", [Message]),
     ranch_memcached_protocol:respond(Conn, ?SUCCESS, #rmp_response{});
+handle(Conn, ?GETKQ, #rmp_message{}=Message) ->
+    handle(Conn, ?GET, Message);
 handle(Conn, ?GET, #rmp_message{key=Key}=Message) ->
-    io:format("Handle ~p~n", [Message]),
+    io:format("Handle GET ~p~n", [Message]),
     ranch_memcached_protocol:respond(Conn, ?SUCCESS, #rmp_response{key=Key, body=Key});
 handle(_Conn, Opcode, Message) ->
     io:format("Oups handle: ~p ~p ~n", [Opcode, Message]).
