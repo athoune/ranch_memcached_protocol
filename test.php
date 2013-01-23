e6d8bb26f9b56a7d3e7e2c9ed46c9cf122698f4c
Mathieu Lecarme <mathieu@garambrogne.net>
2013-01-24 00:03:23 +0100

    Verbose output.

--- test.php
+++ test.php
@@ -2,8 +2,12 @@
 // pecl install memcached
 $m = new Memcached();
 $m->setOption(Memcached::OPT_BINARY_PROTOCOL, True);
+$m->setOption(Memcached::OPT_COMPRESSION, False);
 $m->addServer('localhost', 11211);
 
-$m->set('foo', 100);
+var_dump($m->set('foo', 100));
+var_dump($m->getResultCode());
 var_dump($m->get('foo'));
+var_dump($m->getResultCode());
+// http://php.net/manual/fr/memcached.getresultcode.php
 ?>
