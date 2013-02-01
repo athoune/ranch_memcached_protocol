77cd27e865caf48d80fc04356aa4fa64fd3a2371
Mathieu Lecarme <mathieu@garambrogne.net>
2013-02-02 00:09:23 +0100

    Implementing get command.

--- src/rmp_dummy_plain.erl
+++ src/rmp_dummy_plain.erl
@@ -6,6 +6,19 @@ parseInt(S) ->
     {I, _} = string:to_integer(binary:bin_to_list(S)),
     I.
 
+fromInt(I) ->
+    binary:list_to_bin(io_lib:format("~p", [I])). % It looks very ugly
+
+text(<<"get">>, Keys, _Context, Socket, Transport) ->
+    lists:foreach(fun(Key) ->
+                io:format("Key ~p~n", [Key]),
+                Value = <<"popo">>,
+                Bytes = fromInt(size(Value)),
+                Transport:send(Socket, <<"VALUE ", Key/binary, " 0 ", Bytes/binary, 13, 10, Value/binary, 13, 10>>)
+        end, Keys),
+    Transport:send(Socket, <<"END", 13, 10>>),
+    text;
+
 text(<<"set">>, [Key, _Flags, _Exptime, Bytes], Context, _Socket, _Transport) ->
     Context2 = proplists:compact([{key, Key} | Context]),
     {data, set, parseInt(Bytes), Context2}.
--- test_plain.py
+++ test_plain.py
@@ -6,4 +6,5 @@ mc = memcache.Client(['127.0.0.1:11211'], debug=0)
 
 print mc.set("some_key", "Some value")
 print mc.set("beuha", "Aussi")
-#value = mc.get("some_key")
+value = mc.get("some_key")
+print value
